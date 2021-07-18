e2ed17a479a9a09463f1ae60236791f7869050fb
CAt <80395140+SmokerCat@users.noreply.github.com>
2021-07-18 12:02:12 +0530

    Update song.py

--- VCsMusicBot/modules/song.py
+++ VCsMusicBot/modules/song.py
@@ -33,7 +33,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += " " + str(i)
     print(query)
-    m = message.reply("🔎 Finding the song...")
+    m = message.reply("```🔎 Searching the song...```")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -53,13 +53,13 @@ def song(client, message):
         m.edit("❌ Found Nothing.\n\nTry another keywork or maybe spell it properly.")
         print(str(e))
         return
-    m.edit("Downloading the song ")
+    m.edit("```Downloading and Uploading the song...```")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by @VCsMusicBot**"
+        rep = "**🎵 Uploaded by @Aami_song_bot** \n\nIf you found Any Error please Report [Here](https://t.me/Cat_Telegram_Project_Club) \n\nSong Module Created By [CatX](https://t.me/Cat_of_TelegramX)"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
@@ -74,7 +74,7 @@ def song(client, message):
         )
         m.delete()
     except Exception as e:
-        m.edit("❌ Error")
+        m.edit("```❌ Error...Report Here...[Support](https://t.me/Cat_Telegram_Project_Club)")
         print(e)
 
     try:
