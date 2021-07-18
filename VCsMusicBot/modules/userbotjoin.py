dcc1becc32e57ea3daed722b67f8b870130b972d
CAt <80395140+SmokerCat@users.noreply.github.com>
2021-07-18 12:03:19 +0530

    Update userbotjoin.py

--- VCsMusicBot/modules/userbotjoin.py
+++ VCsMusicBot/modules/userbotjoin.py
@@ -34,7 +34,7 @@ async def addchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @VCsMusicPlayer to your Group and try again</b>",
+            "\n\nOr manually add @CatKing_ext to your Group and try again</b>",
         )
         return
     await message.reply_text(
@@ -95,7 +95,7 @@ async def addcchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "MusicBot"
+        user.first_name = "Aami"
 
     try:
         await USER.join_chat(invitelink)
@@ -109,7 +109,7 @@ async def addcchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel."
-            "\n\nOr manually add @VCsMusicPlayer to your Group and try again</b>",
+            "\n\nOr manually add @CatKing_ext to your Group and try again</b>",
         )
         return
     await message.reply_text(
